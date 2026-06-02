a312cc06c5ac9d5c33631572fa47cb2cca6b31db
Hamidreza <hkamalzade1@gmail.com>
2026-06-02 11:59:37 +0330

    feat(homepage): add React & Next.js promotional section and Feature component

--- src/app/_components/HomeHeroSection/HomeHeroSection.tsx
+++ src/app/_components/HomeHeroSection/HomeHeroSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "../button";
 import Image from "next/image";
+import { IconArrowLeftFill } from "../icons";
 
 const HomeHeroSection = () => {
   return (
@@ -18,9 +19,10 @@ const HomeHeroSection = () => {
             کلاسبن می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه
             هواتو داریم.
           </p>
-          <div className="mt-5 flex gap-4">
-            <Button variant="primary" size="large">
+          <div className="mt-5 flex gap-4 justify-center xl:justify-start">
+            <Button className="animated-icon" variant="primary" size="large">
               دوره‌های ری‌اکت و نکست
+              <IconArrowLeftFill fill="currentColor"/>
             </Button>
             <Button variant="neutral" size="large">
               مشاوره برنامه نویسی
---
+++ src/app/_components/feature/feature.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FeatureProps } from "./feature.types";
+import Image from "next/image";
+
+const Feature: React.FC<FeatureProps> = ({
+  feature: { icon, title, description },
+}) => {
+  return (
+    <article className="flex-1 flex flex-col items-center lg:items-start gap-4">
+      <Image src={icon} alt="" width={52} height={52} />
+      <h4 className="text-lg font-bold">{title}</h4>
+      <p className="max-w-md text-lg text-center lg:text-right">
+        {description}
+      </p>
+    </article>
+  );
+};
+
+export default Feature;
