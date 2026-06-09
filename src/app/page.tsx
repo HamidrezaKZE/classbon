cb892b5222dd16d5e9b66431bd720ae57373adaa
Hamidreza <hkamalzade1@gmail.com>
2026-06-09 12:11:08 +0330

    feat(home): add testimonial component to showcase user feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,13 @@ import { IconArrowLeftFill } from "./_components/icons";
 import { BlogPostSummary } from "@/types/blog-post-summary.interface";
 import { BlogPostCardList } from "./(blog)/_components/blog-post-card";
 import { API_URL } from "@/configs/global";
+import { TestimonialList } from "./_components/testimonial/testimonial-list";
+import { testimonials } from "@/data/testimonials";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
-  const res = await fetch(
-    `${API_URL}/courses/newest/${count}`,
-    { next: { revalidate: 60 * 60 * 24 } },
-  );
+  const res = await fetch(`${API_URL}/courses/newest/${count}`, {
+    next: { revalidate: 60 * 60 * 24 },
+  });
   return res.json();
 }
 async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
@@ -106,6 +107,17 @@ export default async function Home() {
         </div>
         <BlogPostCardList posts={newestBlogPosts} />
       </section>
+      <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          تجربه هم مسیرهای کلاسبن
+        </h2>
+        <p className="mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   );
 }
